7316706523f67dbf9472ca984bacf5eae4a83149
Pitanga <lcmaciel99@gmail.com>
2024-11-01 17:49:37 -0300

    Add Player class at models.ts -b

--- pc-backend/src/models.ts
+++ pc-backend/src/models.ts
@@ -40,12 +40,81 @@ export class ChipHolder {
     }
 
     prepareChips(amount: number): void {
-        this.removeChips(amount);
         this.addPendingChips(amount);
+        this.removeChips(amount);
     }
 
     transferChips(target: ChipHolder, amount: number = this.pendingChips): void {
         target.addChips(amount);
         this.removePendingChips(amount);
     }
+}
+
+export class Player extends ChipHolder {
+    private _id: string;
+    private _isPlaying: boolean;
+    private _isDealer: boolean;
+    private _isTurn: boolean;
+
+    constructor(id: string, startingChips: number = 0) {
+        super();
+        this._id = id;
+        this._isPlaying = true;
+        this._isDealer = false;
+        this._isTurn = false;
+        this.chips = startingChips
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    set id(id: string) {
+        this._id = id;
+    }
+
+    get isPlaying(): boolean {
+        return this._isPlaying;
+    }
+
+    startPlaying(): void {
+        this._isPlaying = true;
+    }
+    
+    stopPlaying(): void {
+        this._isPlaying = false;
+    }
+
+    get isDealer(): boolean {
+        return this._isDealer;
+    }
+
+    giveDealer() {
+        this._isDealer = true;
+    }
+
+    removeDealer() {
+        this._isDealer = false;
+    }
+
+    get isTurn(): boolean {
+        return this._isTurn;
+    }
+
+    startTurn(): void {
+        this._isTurn = true;
+    }
+
+    endTurn(): void {
+        this._isTurn = false;
+    }
+
+    finishTurn(pot: Pot) {
+        this.transferChips(pot, this.pendingChips);
+        this.endTurn();
+    }
+}
+
+export class Pot extends ChipHolder {
+
 }
