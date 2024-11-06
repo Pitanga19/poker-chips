8a3b7c5290db64d0cff7111676a21b1760ae4bf6
Pitanga <lcmaciel99@gmail.com>
2024-11-06 20:30:27 -0300

    Modify Player cass by remove isIndex attributes, Pot class by remove pay logic at chipHolder.ts -b

--- pc-backend/src/models/chipHolders.ts
+++ pc-backend/src/models/chipHolders.ts
@@ -60,21 +60,11 @@ export class ChipHolder {
 export class Player extends ChipHolder {
     private _id: string;
     private _isPlaying: boolean;
-    private _isDealer: boolean;
-    private _isSmallBlind: boolean;
-    private _isBigBlind: boolean;
-    private _isTurn: boolean;
-    private _isWinner: boolean;
 
     constructor(id: string, startingChips: number = 0) {
         super();
         this._id = id;
         this._isPlaying = true;
-        this._isDealer = false;
-        this._isSmallBlind = false;
-        this._isBigBlind = false;
-        this._isTurn = false;
-        this._isWinner = false;
         this.chips = startingChips;
     }
 
@@ -83,12 +73,7 @@ export class Player extends ChipHolder {
             id: this._id,
             chips: this._chips,
             pendingChips: this._pendingChips,
-            isPlaying: this._isPlaying,
-            isDealer: this._isDealer,
-            isSmallBlind: this._isSmallBlind,
-            isBigBlind: this._isBigBlind,
-            isTurn: this._isTurn,
-            isWinner: this._isWinner
+            isPlaying: this._isPlaying
         }
     }
 
@@ -110,66 +95,6 @@ export class Player extends ChipHolder {
     
     stopPlaying(): void {
         this._isPlaying = false;
-    }
-
-    get isDealer(): boolean {
-        return this._isDealer;
-    }
-
-    assignDealer() {
-        this._isDealer = true;
-    }
-
-    removeDealer() {
-        this._isDealer = false;
-    }
-
-    get isSmallBlind(): boolean {
-        return this._isSmallBlind;
-    }
-
-    assignSmallBlind() {
-        this._isSmallBlind = true;
-    }
-
-    removeSmallBlind() {
-        this._isSmallBlind = false;
-    }
-
-    get isBigBlind(): boolean {
-        return this._isBigBlind;
-    }
-
-    assignBigBlind() {
-        this._isBigBlind = true;
-    }
-
-    removeBigBlind() {
-        this._isBigBlind = false;
-    }
-    
-    get isTurn(): boolean {
-        return this._isTurn;
-    }
-    
-    startTurn(): void {
-        this._isTurn = true;
-    }
-    
-    endTurn(): void {
-        this._isTurn = false;
-    }
-
-    get isWinner(): boolean {
-        return this._isWinner;
-    }
-
-    makeWinner() {
-        this._isWinner = true;
-    }
-
-    removeWinner() {
-        this._isWinner = false;
     }
 }
 
@@ -196,23 +121,12 @@ export class Pot extends ChipHolder {
     set winnersCount(value: number) {
         this._winnersCount = value;
     }
-
-    defineWinnersCount(players: Player[]): void {
-        this._winnersCount = players.filter(p => p.isWinner).length;
-    }
     
-    payWinners(players: Player[]): void {
-        players.forEach(p => {
-            if (p.isWinner) {
-                this.transferChips(p, Math.floor(this.pendingChips / this._winnersCount));
-            }
-        })
+    payWinners(): void {
+        
     }
 
-    endPot(players: Player[]): void {
-        this.prepareChips();
-        this.defineWinnersCount(players);
-        this.payWinners(players);
-        this.chips = 0;
+    endPot(): void {
+        
     }
 }
