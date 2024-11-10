826be9d2991a07aaaeaf4c4c054a01d26f73401b
Pitanga <lcmaciel99@gmail.com>
2024-11-10 01:08:12 -0300

    Rename text.ts to test.ts -b

--- pc-backend/src/test.ts
+++ pc-backend/src/test.ts
@@ -2,13 +2,11 @@ import { Player, PlayerManager, Pot } from "./models/chipHolders";
 import { Game, HandStage, BettingStageValidator, BettingStage, PositionManager, TurnValidator } from "./models/gameFlow";
 import { ActionSelector, PlayerActions } from "./models/playerActions";
 
-const player1 = new Player('maci');
-const player2 = new Player('luchi');
-const player3 = new Player('hugo');
-const player4 = new Player('ana');
-
+const player1 = new Player('maci', 300);
+const player2 = new Player('luchi', 300);
+const player3 = new Player('hugo', 300);
+const player4 = new Player('ana', 300);
 const playerList = [player1, player2, player3, player4];
 
 const game = new Game();
 game.playerManager.playerList = playerList;
-
