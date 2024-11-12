c4873848fa78da9beb1f20abe98d0ade8a508d02
Pitanga <lemacidev@gmail.com>
2024-11-12 04:40:02 -0300

    Modify Game class by add pot, handStageValidator, actionSelector, playerActions attributes and methods at gameStages.ts -b

--- pc-backend/src/models/gameStages.ts
+++ pc-backend/src/models/gameStages.ts
@@ -1,32 +1,45 @@
 import { BettingStageType } from "../utils/constants";
-import { Player, PlayerManager, Pot } from "./chipHolders";
+import { PlayerManager, Pot } from "./chipHolders";
 import { PositionManager, HandStageValidator, BettingStageValidator, TurnValidator } from "./gameFlow";
+import { ActionSelector, PlayerActions } from "./playerActions";
 
 export class Game {
     private _playerManager: PlayerManager;
+    private _pot: Pot;
+    private _handStageValidator: HandStageValidator;
     private _handStage: HandStage;
     private _bettingStageManager: BettingStageValidator;
     private _bettingStage: BettingStage;
     private _positionManager: PositionManager;
-    private _turnManager: TurnValidator;
+    private _turnValidator: TurnValidator;
+    private _actionSelector: ActionSelector;
+    private _playerActions: PlayerActions;
 
     constructor () {
         this._playerManager = new PlayerManager();
+        this._pot = new Pot();
+        this._handStageValidator = new HandStageValidator();
         this._handStage = new HandStage();
         this._bettingStageManager = new BettingStageValidator();
         this._bettingStage = new BettingStage();
         this._positionManager = new PositionManager();
-        this._turnManager = new TurnValidator();
+        this._turnValidator = new TurnValidator();
+        this._actionSelector = new ActionSelector();
+        this._playerActions = new PlayerActions();
     }
 
     toJSON() {
         return {
             playerManager: this._playerManager,
+            pot: this._pot,
+            handStageValidator: this._handStageValidator,
             handStage: this._handStage,
             bettingStageManager: this._bettingStageManager,
             bettingStage: this._bettingStage,
             positionManager: this._positionManager,
-            turnManager: this._turnManager,
+            turnValidator: this._turnValidator,
+            actionSelector: this._actionSelector,
+            playerActions: this._playerActions,
         }
     }
 
@@ -36,6 +49,22 @@ export class Game {
 
     set playerManager(playerManager: PlayerManager) {
         this._playerManager = playerManager;
+    }
+
+    get pot(): Pot {
+        return this._pot;
+    }
+
+    set pot(pot: Pot) {
+        this._pot = pot;
+    }
+
+    get handStageValidator(): HandStageValidator {
+        return this._handStageValidator;
+    }
+
+    set handStageValidator(handStageValidator: HandStageValidator) {
+        this._handStageValidator = handStageValidator;
     }
 
     get handStage(): HandStage {
@@ -70,12 +99,28 @@ export class Game {
         this._positionManager = positionManager;
     }
 
-    get turnManager(): TurnValidator {
-        return this._turnManager;
+    get turnValidator(): TurnValidator {
+        return this._turnValidator;
     }
 
-    set turnManager(turnManager: TurnValidator) {
-        this._turnManager = turnManager;
+    set turnValidator(turnValidator: TurnValidator) {
+        this._turnValidator = turnValidator;
+    }
+
+    get actionSelector(): ActionSelector {
+        return this._actionSelector;
+    }
+
+    set actionSelector(actionSelector: ActionSelector) {
+        this._actionSelector = actionSelector;
+    }
+
+    get playerActions(): PlayerActions {
+        return this._playerActions;
+    }
+
+    set playerActions(playerActions: PlayerActions) {
+        this._playerActions = playerActions;
     }
 }
 
