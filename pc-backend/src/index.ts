1f6f227a64c68649ea5a9e37ad12b5aa6893e1c9
Pitanga <lemacidev@gmail.com>
2024-11-13 01:45:45 -0300

    Install cors -b

--- pc-backend/src/index.ts
+++ pc-backend/src/index.ts
@@ -1,28 +1,10 @@
 import express from 'express';
 
-// import models
-import { Player, Pot } from './models/chipHolders'
-import { HandStage, BettingStage, PositionManager, TurnValidator } from './models/gameFlow';
-import { ActionSelector, PlayerActions } from './models/playerActions';
-
 const app = express();
 const PORT = 3000;
 
-const pot = new Pot();
-
-const playerLuchi = new Player('luchi');
-const playerMaci = new Player('maci');
-
 app.get('/', (req, res) => {
     res.send('¡Bienvenidos a Poker Chips!');
-});
-
-app.get('/api/pot', (req, res) => {
-    res.json(pot);
-});
-
-app.get('/api/players', (req, res) => {
-    res.json([ playerLuchi, playerMaci ]);
 });
 
 app.listen(PORT, () => {
