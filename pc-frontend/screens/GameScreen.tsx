143c08b0dc06940bdedbfca80495c4c9377fc572
Pitanga <lemacidev@gmail.com>
2024-11-13 06:30:51 -0300

    Modify GameScreen func by add content at GameScreen.tsx -f

--- pc-frontend/screens/GameScreen.tsx
+++ pc-frontend/screens/GameScreen.tsx
@@ -69,6 +69,29 @@ interface Game {
 const GameScreen = () => {
     // fetch values
     const [game, setGame] = useState<Game | null>(null);
+    const playerManager = game?.playerManager;
+    const playerList = playerManager?.playerList;
+    const pot = game?.pot;
+
+    const renderItem = ({ item }: { item: Player}) => {
+        return (
+            <View style={ styles.container }>
+                <View style={ styles.container }>
+                    <Text style={ styles.mainText }>Player info</Text>
+                    <Text style={ styles.mainText }>ID: {item.id}</Text>
+                    <Text style={ styles.mainText }>Chips: {item.chips}</Text>
+                    <Text style={ styles.mainText }>Pending chips: {item.pendingChips}</Text>
+                </View>
+                <View style={ styles.container }>
+                    <Text style={ styles.mainText }>Avalible actions</Text>
+                    <TextInput
+                        style={ styles.input }
+                        placeholder='amount'
+                    />
+                </View>
+            </View>
+        )
+    }
 
     useEffect(() => {
         // Fetch game
@@ -80,6 +103,19 @@ const GameScreen = () => {
 
     return (
         <View style={ styles.main }>
+            <View style={ styles.container }>
+                <Text style={ styles.mainText }>Pot</Text>
+                <Text style={ styles.mainText }>Chips: {pot?.chips}</Text>
+            </View>
+            <View style={ styles.container }>
+                <Text style={ styles.mainText }>Players</Text>
+                <FlatList
+                    data={ playerList }
+                    renderItem={ renderItem }
+                    keyExtractor={ (item) => item.id }
+                    style={ styles.listContainer }
+                />
+            </View>
         </View>
     );
 };
