9bae2a206036a79046795ae530d2dd44128d4084
Pitanga <lcmaciel99@gmail.com>
2024-11-19 19:47:53 -0300

    Moidify winnerSelect func by remove fetch to winnerSelect endpoint -f

--- pc-frontend/screens/GameScreen.tsx
+++ pc-frontend/screens/GameScreen.tsx
@@ -62,35 +62,15 @@ const GameScreen = () => {
         };
     };
 
-    const winnerSelect = async () =>{
-        try {
-            if (!potList || potList.length === 0) {
-                Alert.alert('Error', 'Not pot founded.');
-                return;
-            }
+    const winnerSelect = () =>{
+        if (!potList || potList.length === 0) {
+            Alert.alert('Error', 'Not pot founded.');
+            return;
+        }
 
-            const response = await fetch(`http://${IP}:${PORT}/api/winnerSelect`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ potList })
-            });
-
-            if (!response.ok){
-                const errorData = await response.json();
-                throw new Error(errorData?.message || 'Failed to set a winner selection.');
-            };
-
-            const data = await response.json();
-            console.log('Pots waiting for winners:', data);
-            navigation.navigate('WinnerSelect');
-        } catch (error: unknown) {
-            if (error instanceof Error) {
-                Alert.alert('¡Error!', error.message);
-            } else {
-                Alert.alert('¡Error!', 'Unknown error.');
-            };
-        };
-    }
+        console.log('Pots waiting for winners:', potList);
+        navigation.navigate('WinnerSelect');
+    };
 
     useEffect(() => {
         fetchGameData();
